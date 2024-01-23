8eb67f633fd8e0e5719abaa881cabb3be9cbaae7
username <50456872+akshatkh@users.noreply.github.com>
2024-01-23 09:32:47 -0800

    new

--- quiz1.sh
+++ quiz1.sh
@@ -1,2 +1,15 @@
 echo "Akshat Khandelwal"
 echo $USER
+
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "a" | grep -v "[^acfmotu]"| grep -E ".{4}" | wc -l
+
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "b" | grep -v "[^abilnrt]"| grep -E ".{4}" | wc -l
+
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "c" | grep -v "[^acdimno]"| grep -E ".{4}" | wc -l
+
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "z" | grep -v "[^aginorz]"| grep -E ".{4}" | wc -l
+
+
+gunzip -c ~/Code/MCB185/data/jaspar2024_core.transfac.gz | grep -v "^#"| cut -f 3 | sort -n | uniq -c | grep "tax_group"
+
+echo "Yutong and Roger"
