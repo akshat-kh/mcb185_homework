a50d6c285b2e1e65d3fb695f3e4dbd65841a9b5d
username <50456872+akshatkh@users.noreply.github.com>
2024-03-20 15:21:41 -0700

    new

--- 74genefinder.py
+++ 74genefinder.py
@@ -4,6 +4,7 @@ import mcb185
 import dogma
 import sys
 
+min_orf_len = int(sys.argv[2])
 def get_orfs(fseq, reversed):
 	i = 0
 	orfs = []
@@ -18,7 +19,7 @@ def get_orfs(fseq, reversed):
 			codon = fseq[j:j+3]
 			if codon == 'TAA' or codon == 'TAG' or codon == 'TGA':
 				stop = j
-				if (stop - start) > 1000:
+				if (stop - start) > min_orf_len:
 					if reversed:
 						orfs.append([len(fseq)-i-1-frame, len(fseq)-j-1-frame])
 					else:
@@ -28,14 +29,36 @@ def get_orfs(fseq, reversed):
 		i += 3
 	return orfs
 
-
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
 	rev = dogma.revcomp(seq)
 	for frame in range(3):
-		print('frame ', frame)
+# 		print('frame ', frame)
 		fseq = seq[frame:]
 		rfseq = rev[frame:]
-		print('+ strand: ', get_orfs(fseq, False))
-		print('- strand: ', get_orfs(rfseq, True))
-		print()				
-				
+		# print('+ strand: ', get_orfs(fseq, False))
+# 		print()
+# 		print('- strand: ', get_orfs(rfseq, True))
+		forward_coords = get_orfs(fseq, False)
+		rev_coords = get_orfs(rfseq, True)
+		
+		for i in range(len(forward_coords)):
+			print(f'NC_00913.{i}', end='\t')
+			print('RefSeq', end='\t')
+			print('CDS', end='\t')
+			print(forward_coords[i][0], end='\t')
+			print(forward_coords[i][1], end='\t')
+			print('.', end='\t')
+			print('+', end='\t')
+			print()
+
+		for j in range(len(rev_coords)):
+			print(f'NC_00913.{j}', end='\t')
+			print('RefSeq', end='\t')
+			print('CDS', end='\t')
+			print(forward_coords[j][0], end='\t')
+			print(forward_coords[j][1], end='\t')
+			print('.', end='\t')
+			print('-', end='\t')
+			print()
+			
+							
